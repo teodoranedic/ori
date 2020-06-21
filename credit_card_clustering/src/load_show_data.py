791ef7619f025c2307dfec33f55811cfacbfd91d
teodoranedic <nedicteodora1@gmail.com>
2020-06-21 23:17:59 +0200

    kmeans algoritam

--- credit_card_clustering/src/load_show_data.py
+++ credit_card_clustering/src/load_show_data.py
@@ -2,35 +2,52 @@
 import pandas
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # ucitavanje podataka
 
 def load_data():
-    data = pandas.read_csv("../data/credit_card_data.csv")
+    data = pandas.read_csv("C:\\Users\\teodo\Desktop\ori\credit_card_clustering\data\credit_card_data.csv")
+    #data = pandas.read_csv("../data/credit_card_data.csv")
     data = data.fillna(data.median())
-    return data
+    vals = data.iloc[:, 1:].values
+    return data, vals
 
 
 # odredjivanje broja klastera
 
-def number_cluster(data):
-    vals = data.iloc[:, 1:].values
+def number_cluster(vals):
     wcss = []
-    for ii in range(1, 50):
+    for ii in range(1, 12):
         kmeans = KMeans(n_clusters=ii, init="k-means++", n_init=10, max_iter=300)
-        kmeans.fit_predict( vals )
+        kmeans.fit_predict(vals)
         wcss.append( kmeans.inertia_ )
     plt.plot(wcss, 'ro-', label="WCSS")
-    plt.title("Computing WCSS for KMeans++")
     plt.xlabel("Number of clusters")
     plt.ylabel("WCSS")
     plt.show()
 
-if __name__ == '__main__':
-    data = load_data()
+def kmeans_function(data, vals):
+    kmeans = KMeans(n_clusters=10, init="k-means++", n_init=10, max_iter=300)
+    y_pred = kmeans.fit_predict(vals)
+    # for i in y_pred:
+    #     print(i)
+    # print(len(y_pred))
+    data["cluster"] = y_pred
+    cols = list(data.columns)
+    cols.remove("CUST_ID")
 
-    number_cluster(data)
+    # sns.pairplot(data[cols], hue="cluster")
+    # plt.show()
+
+
+if __name__ == '__main__':
+    data, vals = load_data()
+    #number_cluster(vals)
     # sa crteza uzimamo da je broj klastera 10
     k = 10
+    kmeans_function(data, vals)
+
+
 
